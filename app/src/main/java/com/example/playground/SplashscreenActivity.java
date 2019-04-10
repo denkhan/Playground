d11ff038fea1a560d8ca699e74593ed1e0b6c490
denkhan <d.lunde@hotmail.com>
2019-04-10 14:38:00 +0200

    Splash screen works

---
+++ app/src/main/java/com/example/playground/SplashscreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.playground;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(1000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
